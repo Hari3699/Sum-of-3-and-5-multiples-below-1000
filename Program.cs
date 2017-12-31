1ea1298c2a60ff124002508c3a1e1d4d6ee05df7
Hari3699 <33191254+Hari3699@users.noreply.github.com>
2017-12-30 18:41:24 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfMultiplesOf3And5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            for (int i = 3; i < number ; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+
+            Console.WriteLine("sum of Multiples : {0}", sum);
+        }
+    }
+}
